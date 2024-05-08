02969db40bdb533d83458ed7cade657d3496968d
Aslan <117452702+AslanRasl@users.noreply.github.com>
2024-05-08 16:01:06 +0500

    Update downloaded.jsx

--- src/components/downloaded/downloaded.jsx
+++ src/components/downloaded/downloaded.jsx
@@ -1,13 +1,12 @@
 import "./downloaded.scss"
-import down from "./down.png"
 function Downloaded() {
     return(
-        <div className="hero__down">
+        <div className="hero__down container2">
             
             <div className="hero__sub__down">
             <h1 className="title__down">20M+ downloaded from 32 diffrent countires</h1>
             <p className="subtitle__down">Try demo for 7 days with full features.</p>
-            <button className="btn__down">Try free demo <img src={down} alt="" /></button>
+            <button className="btn__down">Try free demo⠀↓</button>
             </div>
         </div>
     )
